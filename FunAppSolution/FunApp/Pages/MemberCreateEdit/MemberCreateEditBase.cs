77e8fdc7de728a3812c997cb4c0231595c0623b3
Ashish Kumar Agarwal <AshishKumar.Agarwal@csgi.com>
2020-07-29 20:24:44 +0530

    Added Creating logic for Team member

---
+++ FunAppSolution/FunApp/Pages/MemberCreateEdit/MemberCreateEditBase.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using FunApp.Common.Models;
+using FunApp.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace FunApp.Pages.MemberCreateEdit
+{
+    public class MemberCreateEditBase : ComponentBase
+    {
+        [CascadingParameter]
+        public Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        [Inject]
+        public ITeamMemberService TeamMemberService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public string PageHeaderText { get; set; }
+
+        public TeamMember TeamMember { get; set; } = new TeamMember();
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            int.TryParse(Id, out int employeeId);
+
+            if (employeeId != 0)
+            {
+                PageHeaderText = "Edit Member";
+                TeamMember = await TeamMemberService.GetTeamMember(int.Parse(Id));
+            }
+            else
+            {
+                PageHeaderText = "Create Member";
+                TeamMember = new TeamMember();
+            }
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            TeamMember result = null;
+
+            if (TeamMember.Id != 0)
+            {
+                result = await TeamMemberService.UpdateTeamMember(TeamMember);
+            }
+            else
+            {
+                result = await TeamMemberService.CreateTeamMember(TeamMember);
+            }
+            if (result != null)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
