578e049f8ba06fa2635c964f197f9168117fdc88
s0111991 <68818223+s0111991@users.noreply.github.com>
2020-07-30 17:08:24 +0530

    pages project details

---
+++ FunAppSolution/FunApp/Pages/ProjectDetails/ProjectDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FunApp.Common.Models;
+using FunApp.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace FunApp.Pages.ProjectDetails
+{
+    public class ProjectDetailsBase : ComponentBase
+    {
+        [Inject]
+        public ITeamMemberService TeamMemberService { get; set; }
+
+        public IEnumerable<TeamMember> Members { get; set; }
+        public ProjectDetailList _projectDetail;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Members = (await TeamMemberService.GetTeamMembers()).ToList();
+            _projectDetail = new ProjectDetailList();
+        }
+    }
+}
