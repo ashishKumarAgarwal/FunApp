560fa0b6ad242a7dad7b5f7aabf5d3f4b6599f4b
missar01 <Saroj.Mishra@csgi.com>
2020-07-31 01:06:22 +0530

    Document page changes

--- FunAppSolution/FunApp.Common/Models/Doc.cs
+++ FunAppSolution/FunApp.Common/Models/Doc.cs
@@ -5,10 +5,12 @@ namespace FunApp.Common.Models
 {
     public class Doc
     {
-        public Dictionary<string,string> DocumentNameWithLink { get; set; } = new Dictionary<string, string>() { {"Demo" , "https://web.microsoftstream.com/embed/video/81ee3c54-61fe-42e3-9522-aa99f71d1f62?autoplay=false&amp;showinfo=true" },
-            {"About CSG", "https://web.microsoftstream.com/embed/video/81ee3c54-61fe-42e3-9522-aa99f71d1f62?autoplay=false&amp;showinfo=true" },
-            {"DNA DW OVerView","https://web.microsoftstream.com/embed/video/5ddc6818-e171-4533-a3f1-1099a04cd16b?autoplay=false&amp;showinfo=true" },
-            { "Snowflake Inro","https://www.youtube.com/embed/dZlBCLLL7UA?autoplay;encrypted-media;gyroscope;picture-in-picture"}
+        public Dictionary<string,string> DocumentNameWithLink { get; set; } = new Dictionary<string, string>() { {"Replicate Architecture" , "https://confluence.csgicorp.com/display/VT/DNA+Deployment+Architecture" },
+            {"Replicate Environment Details", "https://confluence.csgicorp.com/display/VT/DNA+Replicate+Environments" },
+            {"Db2 Password Rotation Steps","https://confluence.csgicorp.com/display/VT/IBM+DB2+password+rotation" },
+            { "Snowflake Password Rotation Steps","https://confluence.csgicorp.com/display/VT/Snowflake+password+rotation"},
+            { "Rundeck Sandbox","https://rundecksb.csgidev.com/user/login"},
+            { "Rundeck Non-Prod","https://rundeck.csgidev.com/user/login"}
         };
 
     }
