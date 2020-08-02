2e4c231de0952792a2422edb2a47b96a730aabb0
Ashish Kumar Agarwal <AshishKumar.Agarwal@csgi.com>
2020-08-02 21:14:27 +0530

    Updated Service call for subject area

--- FunAppSolution/FunApp/Services/SubjectAreaService.cs
+++ FunAppSolution/FunApp/Services/SubjectAreaService.cs
@@ -1,7 +1,6 @@
 ﻿using FunApp.Common.Models;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -19,56 +18,12 @@ namespace FunApp.Services
 
         public async Task<IEnumerable<SubjectArea>> GetSubjectAreas()
         {
-            //return await _httpClient.GetJsonAsync<IEnumerable<SubjectArea>>(BaseApi);
-            return new List<SubjectArea>()
-            {
-                new SubjectArea()
-                {
-                        Name = "Snowflake",
-                         SubjectAreaId = 1,
-                    },
-                new SubjectArea()
-                {
-                        Name = "Rundeck",
-                         SubjectAreaId = 2,
-                    },
-                new SubjectArea()
-                {
-                        Name = "Attunity",
-                         SubjectAreaId = 3,
-                    } ,
-                new SubjectArea()
-                {
-                        Name = "Chef",
-                         SubjectAreaId = 4,
-                    }};
+            return await _httpClient.GetJsonAsync<IEnumerable<SubjectArea>>(BaseApi);
         }
 
         public async Task<IEnumerable<SubjectArea>> GetSubjectArea(int SubjectAreaId)
         {
-            return new List<SubjectArea>()
-            {
-                new SubjectArea()
-                {
-                        Name = "Snowflake",
-                         SubjectAreaId = 1,
-                    },
-                new SubjectArea()
-                {
-                        Name = "Rundeck",
-                         SubjectAreaId = 2,
-                    },
-                new SubjectArea()
-                {
-                        Name = "Attunity",
-                         SubjectAreaId = 3,
-                    },
-                new SubjectArea()
-                {
-                        Name = "Chef",
-                         SubjectAreaId = 4,
-                    } }.Where(doc => doc.SubjectAreaId == SubjectAreaId);
-            //return await _httpClient.GetJsonAsync<IEnumerable<SubjectArea>>($"{BaseApi}/{SubjectAreaId}");
+            return await _httpClient.GetJsonAsync<IEnumerable<SubjectArea>>($"{BaseApi}/{SubjectAreaId}");
         }
     }
 }
